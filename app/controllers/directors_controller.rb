880625cff2f29966b4f149c57804cfb23f3a2121
yingjiac <yingjiac@uchicago.edu>
2022-05-04 06:08:40 +0000

    Half way of this assignment

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,23 @@
 class DirectorsController < ApplicationController
   def index
+    @list_of_directors = Director.all
+
     render({ :template => "director_templates/index.html.erb"})
   end  
 
+  def youngest
+    @youngest= Director.order({ :dob => :desc}).at(0)
+    render({ :template => "director_templates/youngest.html.erb"})
+  end 
+
+  def wisest
+    @oldest= Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+    render({ :template => "director_templates/eldest.html.erb"})
+  end 
+  
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id}).at(0)
+    render({ :template => "director_templates/show.html.erb"})
+  end
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+  get("/directors/youngest", { :controller => "directors", :action => "youngest"})
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
 end
